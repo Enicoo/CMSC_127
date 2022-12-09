3504e65f4285736ae30376108a7979b4a50b29b5
Enicoo <100269437+Enicoo@users.noreply.github.com>
2022-12-09 15:55:00 +0800

    Update demo.php
    Nilagay ko yung connection sa seperate php file and gumawa ako ng search button

--- demo.php
+++ demo.php
@@ -1,55 +1,51 @@
+<?php
+	include 'connect.php'
+?>
 <html>
 	<body>
 
-		<table border="1">
-		<tr>
-			<th>Personal ID</th>
-			<th>Name</th>
-			<th>Course Number</th>
-		</tr>
-	
+		<!-- search function-->
+		<form action = "search.php" method ="POST">
+			<input type="text" name = "search" placeholder="Try searching" >
+			<button type ="submit" name ="submit-search">Enter</button>
+		</form>
+
+
+
+		<!-- Nothing special just codes to display a table [can be removed]-->
+		<div id="table">
+				<table border="1">
+						<tr>
+							<th>Personal ID</th>
+							<th>Name</th>
+							<th>Course Number</th>
+						</tr>
+						
+						<?php
+
+							$sql1 = "SELECT * from personaldata2";
+							$result = $conn->query($sql1);
+
+							if ($result->num_rows > 0) {
+								// output data of each row
+								while($row = $result->fetch_assoc()) {
+								echo "<tr><td>" . 
+								$row["p_ID"]. "</td><td>" . 
+								$row["p_name"]. "</td><td>" . 
+								$row["c_ID"]. "</td></tr>";
+
+								}
+								echo "</table>";
+							} else {
+								echo "0 results";
+							}
+							$conn->close();
+						?>
+
+
+		</div>
+				
+
 
 	</body>
-
 </html>
-
-
-
-
-
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "database127";
-	
-
-	//Ths is the connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-
-	//Check Connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-		
-	}
-	echo "The db is connected successfully";
-
-	$sql = "SELECT * from personaldata2";
-	$result = $conn->query($sql);
-
-	if ($result->num_rows > 0) {
-		// output data of each row
-		while($row = $result->fetch_assoc()) {
-		  echo "<tr><td>" . $row["p_ID"]. "</td><td>" . $row["p_name"]. "</td><td>" . $row["c_ID"]. "</td></tr>";
-
-		}
-		echo "</table>";
-	  } else {
-		echo "0 results";
-	  }
-	  $conn->close();
-
-?>
-
