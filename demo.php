18efbc5fea8649140b36c1d28869f0f7668738ce
Enicoo <100269437+Enicoo@users.noreply.github.com>
2022-12-12 15:56:17 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,71 +1,65 @@
 <?php
 	include 'connect.php'
 ?>
-<html>
-	<head>
-		<link  rel="stylesheet" type="text/css" href="style.css" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<html lang=en>
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> HMMbois - School Library </title> 
+	<link  rel="stylesheet" type="text/css" href="style.css" />
 
+	<!-- Usage of Online Resources -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+	<!---- Header section ---->
+	<section class="header">
 		<div class="navbar">
 				<div class = "logo">
-					<a href="#">Library Database</a>
+					<img src="img/logo.png">
 				</div>
-		
-			</div>
-		
-
-	</head>
-	<body>
-
-	<div class="searchbtn">
-			<form action = "search.php" method ="POST" >
-				<input type="text" name = "search" placeholder="Do you want to find something?" class="searchbar">
-				<button type ="submit" name ="submit-search" class="submitbtn"><i class="fa fa-search"></i></button>
-			</form>
-	</div>
-			<!-- search function-->
-			
-
-
-
-		<!-- Nothing special just codes to display a table [can be removed]-->
-		<div class="table">
-				<table border="1">
-						<tr>
-							<th>Personal ID</th>
-							<th>Name</th>
-							<th>Course Number</th>
-						</tr>
-						
-						<?php
-
-							$sql1 = "SELECT * from personaldata";
-							$result = $conn->query($sql1);
-
-							if ($result->num_rows > 0) {
-								// output data of each row
-								while($row = $result->fetch_assoc()) {
-								echo "<tr><td>" . 
-								$row["p_ID"]. "</td><td>" . 
-								$row["p_name"]. "</td><td>" . 
-								$row["c_ID"]. "</td></tr>";
-
-								}
-								echo "</table>";
-							} else {
-								echo "0 results";
-							}
-							$conn->close();
-						?>
-
-
+			<nav>
+				<ul>
+					<li><a href="demo.php">Home</a></li>
+					<li><a href="#btn">Search</a></li>
+				</ul>
+			</nav>
 		</div>
-				
+	</section>
 
+	<!--- First section --->
+	<section class="section1">
+		<div class="sec1container">
+			<div class="row">
+				<div class="col-2">
+				<h1>Welcome to UP Baguio Library </h1>
+				</div>
+				<div class="col-2">
+					<img src="img/sec1logo.png" class="sec1logo">
+				</div>
+				<div class="searchbtn" id= "btn">
+					<form action = "search.php" method ="POST" >
+						<input type="text" name = "search" placeholder="Do you want to find something?" class="searchbar">
+						<button type ="submit" name ="submit-search" class="submitbtn"><i class="fa fa-search"></i></button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
 
-	</body>
-</html>
-
+		<!-- search function-->
+		<!-- Nothing special just codes to display a table [can be removed]-->
+		
+		<section class="footer">
+			<div class="footer-logo">
+				<img src="img/sec1logo.png">
+			</div>
+			<div class="footer-about">
+				<p>© 2022 HMM School Library, All Rights Reserved</p>
+			</div>
+    	</section>
 	</body>
 </html>
